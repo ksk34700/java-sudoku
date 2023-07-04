884b6db6069cdc528b31de06847fb262bc9db6b8
unknown <ksk34700@gmail.com>
2023-07-04 08:58:34 -0700

    added box

--- sudoku-app/src/main/java/com/drak/apps/sudoku/Sudoku.java
+++ sudoku-app/src/main/java/com/drak/apps/sudoku/Sudoku.java
@@ -7,8 +7,8 @@ public class Sudoku {
 
 	private int[][] sudoku;
 
-	public static final int ROWS = 9;
-	public static final int COLS = 9;
+	public static final int ROWS = 9, COLS = 9;
+	public static final int BOX = 3;
 
 	private Sudoku() {
 	}
@@ -44,7 +44,6 @@ public class Sudoku {
 
 	public Sudoku clone() {
 		Sudoku clone = new Sudoku();
-
 		clone.sudoku = new int[9][];
 
 		for (int i = 0; i < ROWS; i++) {
@@ -55,7 +54,7 @@ public class Sudoku {
 		}
 		return clone;
 	}
-	
+
 	public void transpose() {
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = i + 1; j < COLS; j++) {
@@ -64,6 +63,31 @@ public class Sudoku {
 				sudoku[j][i] = temp;
 			}
 		}
+	}
+
+	public int[][] box(int r, int c) {
+		int[][] box = new int[BOX][];
+		int row = BOX * (r);
+		int col = BOX * (c);
+		for (int i = 0; i < BOX; i++) {
+			box[i] = new int[BOX];
+			for (int j = 0; j < BOX; j++) {
+				box[i][j] = sudoku[row + i][col + j];
+			}
+		}
+		return box;
+	}
+
+	public static void print(int[][] box) {
+		for (int i = 0; i < box.length; i++) {
+			int[] cols = box[i];
+			for (int j = 0; j < cols.length; j++) {
+				System.out.print(box[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println(" ");
+		}
+		System.out.println(" ");
 	}
 
 	public void print() {
@@ -87,17 +111,44 @@ public class Sudoku {
 	}
 
 	public boolean isSolved() {
-		/*for (int i = 0; i < ROWS; i++) {
+		boolean solved=true;
+		for (int i = 0; i < ROWS; i++) {
 			if (!checkRows(i)) {
+				solved=false;
 				System.out.println("Not solved for row " + i);
 			}
-		}*/
+		}
 		for (int j = 0; j < ROWS; j++) {
 			if (!checkCols(j)) {
+				solved=false;
 				System.out.println("Not solved for col " + j);
 			}
 		}
+		for (int i = 0; i < BOX; i++) {
+			for (int j = 0; j < BOX; j++) {
+				int[][] box = box(i, j);
+				if (!checkBox(box)) {
+					solved=false;
+					System.out.println("Not solved for Box " + i + "," + j);
+				}
+			}
 
+		}
+		return solved;
+	}
+
+	private boolean checkBox(int[][] box) {
+		Set<Integer> values = new HashSet<>();
+		values.add(0);
+		for (int i = 0; i < BOX; i++) {
+			for (int j = 0; j < BOX; j++) {
+				if (values.contains(box[i][j])) {
+					return false;
+				} else {
+					values.add(box[i][j]);
+				}
+			}
+		}
 		return true;
 	}
 
@@ -129,17 +180,37 @@ public class Sudoku {
 
 	public static void main(String[] s) {
 		Sudoku sudoku = new Sudoku(
-				"123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 ");
-		sudoku.print();
+				"123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789");
+		//sudoku.print();
 		System.out.println(" ");
-		 
-		Sudoku clone = sudoku.clone();
+
+		Sudoku sudoku1 = new Sudoku(
+				  "326984157 "
+				+ "981657423 "
+				+ "475132968 "
+				+ "258473619 "
+				+ "764519382 "
+				+ "193826745 "
+				+ "642398571 "
+				+ "819745236 "
+				+ "537261894");
+		//sudoku1.print();
+		//System.out.println(sudoku1.isSolved());
 		
-		clone.transpose();
-		clone.print();
 		
-		System.out.println(clone.isSolved());
-		
+		Sudoku s2 = new Sudoku(
+				  "634100527 "
+				+ "075032000 "
+				+ "890500003 "
+				+ "357080200 "
+				+ "408000709 "
+				+ "001020834 "
+				+ "000006045 "
+				+ "000470980 "
+				+ "243009176");
+		s2.print();
+		System.out.println(s2.isSolved());
+
 	}
 
 }
